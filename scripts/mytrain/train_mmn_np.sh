cd877f671a063ed5421d911ceee5bedfe692097d
MstXy <cz1627@nyu.edu>
2022-08-01 11:42:49 +0800

    shn_pw exp, no gpu preference

---
+++ scripts/mytrain/train_mmn_np.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#SBATCH --job-name=fs_val
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=32GB
+#SBATCH --time=48:00:00
+#SBATCH --mail-type=END
+#SBATCH --mail-user=cz1627@nyu.edu
+#SBATCH --output=seg.out
+#SBATCH --gres=gpu # How much gpu need, n is the number
+
+
+
+module purge
+
+DATA=$1
+SPLIT=$2
+LAYERS=$3
+SHOT=$4
+
+
+
+echo "start"
+singularity exec --nv \
+            --overlay /scratch/cz1627/overlay-25GB-500K.ext3:ro \
+			/scratch/cz1627/cuda11.4.2-cudnn8.2.4-devel-ubuntu20.04.3.sif \
+            /bin/bash -c " source /ext3/env.sh;
+            python -m src.train_kshot_self --config config_files/${DATA}_mmn_check.yaml \
+					 --opts train_split ${SPLIT} \
+						    layers ${LAYERS} \
+						    shot ${SHOT} \
+					 > log_shn.txt 2>&1"
+
+echo "finish"
+
+
+#GREENE GREENE_GPU_MPS=yes
+
