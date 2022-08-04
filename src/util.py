71d6bb10b565484a6721d000983f7fb2609d1f70
MstXy <cz1627@nyu.edu>
2022-08-04 13:27:54 +0800

    data augmentation

--- src/util.py
+++ src/util.py
@@ -17,6 +17,16 @@ A = TypeVar("A")
 B = TypeVar("B")
 
 
+def tensor_slice(x, idx=None, ref=None):    # idx is 0/1, ref is tensor on gpu
+    n, c, h, w = x.shape
+    x = x.view(n // 2, 2, c, h, w)
+    if idx is not None:
+        out = x[:, idx, :, :, :]
+    elif ref is not None:
+        ref = ref.reshape((len(ref)//2, 2))
+        indices = torch.argmax(ref, dim=-1)
+        out = x[ torch.arange(n//2, device=indices.device), indices ]
+    return out
 
 def get_aux_loss(wt, att_q, f_q, q_label, model, eps=0.6, reduction='mean'):
     pd0 = F.softmax(model.classifier(att_q), dim=1)   # [1, 2, 60, 60]
